98951029335614b68ce3d5519a1502d1348907eb
PhutBot <phutbot@gmail.com>
2021-12-29 15:56:00 -0600

    pls

---
+++ index.js
@@ -0,0 +1,15 @@
+import * as Env from './dist/Env';
+import * as File from './dist/File';
+import * as Https from './dist/Https';
+import * as Millis from './dist/Millis';
+import * as Rand from './dist/Rand';
+import * as Serializable from './dist/Serializable';
+
+module.exports = {
+    Env,
+    File,
+    Https,
+    Millis,
+    Rand,
+    Serializable,
+};
