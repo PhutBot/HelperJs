73ccd8b10afe48d4534d1c224a4b55c08bd1ae1b
PhutBot <phutbot@gmail.com>
2022-01-21 00:00:38 -0600

    add dist files

--- dist/src/Https/SimpleServer.js
+++ dist/src/Https/SimpleServer.js
@@ -93,13 +93,20 @@ class SimpleServer {
                     }
                     file = fs.readFileSync(`./${path}`);
                 }
-                else if (stat.isDirectory() && fs.existsSync(`./${path}/index.html`)) {
-                    file = fs.readFileSync(`./${path}/index.html`, 'utf8');
+                else if (stat.isDirectory()) {
+                    if (fs.existsSync(`./${path}/index.html`)) {
+                        res.setHeader('content-type', 'text/html');
+                        file = fs.readFileSync(`./${path}/index.html`, 'utf8');
+                    }
+                    else if (fs.existsSync(`./${path}/index.js`)) {
+                        res.setHeader('content-type', 'application/javascript');
+                        file = fs.readFileSync(`./${path}/index.js`, 'utf8');
+                    }
                 }
                 else {
                     throw new Error('how is this not a file or a directory??');
                 }
-                if (this.useCache && !!options.cache) {
+                if (this.useCache && !!options.cache && !!file) {
                     this.cachedFiles[path] = file;
                 }
             }
